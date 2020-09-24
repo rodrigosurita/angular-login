d319005c1c2ca0fa5f7cd9dcad091e7986c3f695
Rodrigo <rodrigosurita@live.com>
2020-09-24 14:24:06 -0300

    feat: login is now creating session

--- backend/controller.js
+++ backend/controller.js
@@ -1,15 +1,31 @@
+/**
+ * Check the user's information and create a session if the user is authenticated
+ *
+ * @param {object} req - request object
+ * @param {object} res - response object
+ */
 exports.loginSubmit = async (req, res) => {
-  console.log(req.body);
   let { username, password } = req.body;
   if(username == 'admin' && password == 'admin'){
+
+    //creating user session - example
+    req.session.user = {
+      name: 'ADMIN',
+      address: 'Sao Paulo, SP, Brazil',
+      job: 'developer'
+    }
+
     res.status(200).json({
       success: true,
       message: 'OK'
     });
+
   } else {
+
     res.status(200).json({
       success: false,
       message: 'Incorrect username or password.'
     });
+
   }
 };
